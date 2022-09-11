43b834189b75a63be04a85963317408146cdfa0a
Foguel <gustavofoguel123@outlook.com>
2022-09-11 17:20:19 -0300

    Initial Commit

---
+++ TabelaFifaWorldCup2022/TabelaFifaWorldCup2022/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabelaFifaWorldCup2022
+{
+    public partial class MainPage : ContentPage
+    {
+        TelaJogos telaJogos;
+
+        public MainPage()
+        {
+            this.BindingContext = this;
+            InitializeComponent();
+            telaJogos = new TelaJogos();
+        }
+
+        public bool TelaJogosConficao { get; set; }
+
+
+
+        private async void BtnEntrarMenu(object sender, EventArgs e)
+        {
+            TelaJogosConficao = true;
+            await Task.Delay(300);
+            TelaJogosConficao = false;
+
+            await Navigation.PushAsync(telaJogos);
+        }
+    }
+}
---
+++ TabelaFifaWorldCup2022/TabelaFifaWorldCup2022/TelaJogos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabelaFifaWorldCup2022
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TelaJogos : ContentPage
+    {
+        public TelaJogos()
+        {
+            this.BindingContext = this;
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
